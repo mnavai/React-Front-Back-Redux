aba47b403fd7b479f446ca95fccc8444dc750851
Maryam Navaei <maryam.navaei@gmail.com>
2023-05-02 12:31:00 -0400

    login finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import RegisterPage from './pages/register.page';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/register.page";
+import LoginPage from "./pages/login.page";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<RegisterPage></RegisterPage>}></Route>
-        <Route path='verifyemail'>
-          <Route path=':verificationCode' ></Route>
+        <Route path="/" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="verifyemail">
+          <Route path=":verificationCode" />
         </Route>
       </Routes>
     </div>
